b33e0837b05a69cbd061abea5636ed7fa651934d
Claudiu Filip <claudiu.Filip@ness.com>
2018-04-19 15:53:43 +0300

    Adds react router with 2 dummy pages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Loader from './containers/Loader';
+import Home from './containers/Home';
+
 import 'todomvc-common/base';
 import 'todomvc-common/base.css';
 import 'todomvc-app-css';
+
 import 'app.css';
 
 export default () => {
-	return <div>Hello World!</div>
+	return <BrowserRouter>
+		<div>
+			<Route exact path="/" component={Loader} />
+			<Route path="/home" component={Home} />
+		</div>
+	</BrowserRouter>
 };
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
 		]
 	},
 	devServer: {
+		historyApiFallback: true,
 		contentBase: './dist',
 		hot: true,
 		inline: true
